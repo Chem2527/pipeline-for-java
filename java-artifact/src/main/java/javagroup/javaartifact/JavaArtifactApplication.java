9a5deb125ee0dbd642a451feb724914d6d49c5b4
Saikrishna <108404892+Chem2527@users.noreply.github.com>
2023-12-13 15:30:07 +0530

    m1

---
+++ java-artifact/src/main/java/javagroup/javaartifact/JavaArtifactApplication.java
@@ -0,0 +1,13 @@
+package javagroup.javaartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaArtifactApplication.class, args);
+	}
+
+}
